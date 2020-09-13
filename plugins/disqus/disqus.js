d9e71ab223c8106b2c601c6a65cc9fb24820b8fc
Ahmad Hejazee <mngafa@gmail.com>
2020-09-13 16:35:27 +0430

    p

--- plugins/disqus/disqus.js
+++ plugins/disqus/disqus.js
@@ -2,16 +2,18 @@ var disqus_config = function () {
   this.language = "fa";
 };
 (function($) {
-  $(function() {          
-    (function() {
-      if (["localhost", "127.0.0.1"].indexOf(window.location.hostname) != -1) {
-        document.getElementById('disqus_thread').innerHTML = 'Disqus comments not available by default when the website is previewed locally.';
-        return;
-      }
-      var d = document, s = d.createElement('script'); s.async = true;
-      s.src = '//' + 'hejazee.disqus.com/embed.js';
-      s.setAttribute('data-timestamp', + new Date());
-      (d.head || d.body).appendChild(s);
-    })();
+  $(function() {  
+    if ($('#disqus_thread').length > 0) {
+      (function() {
+        if (["localhost", "127.0.0.1"].indexOf(window.location.hostname) != -1) {
+          document.getElementById('disqus_thread').innerHTML = 'Disqus comments not available by default when the website is previewed locally.';
+          return;
+        }
+        var d = document, s = d.createElement('script'); s.async = true;
+        s.src = '//' + 'hejazee.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', + new Date());
+        (d.head || d.body).appendChild(s);
+      })();
+    }
   });
 })(jQuery);
